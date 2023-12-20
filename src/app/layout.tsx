b4111ec9cbaede6d7085245603a6bca48fd7c4a8
배성완 <baesee@kakao.com>
2023-12-20 17:19:50 +0900

    setting : globalcss reset, font (Noto_Sans_KR) 설정
    - layoutType을 BasicLayoutType으로 지정했습니다. layout.tsx 생성시에 기본이면 넣어주세요!
    - font는 Noto Sans KR로 지정했습니다. 구글 폰트입니다.
    - css reset 했습니다. styles/global.css 에서 확인 가능합니다.
    
    Relate to : #7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,15 @@
+import { BasicLayoutType } from '@/types/layoutType';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import { Noto_Sans_KR } from 'next/font/google';
+import '@/styles/global.css';
+const inter = Noto_Sans_KR({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: '',
 	description: '',
 };
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: BasicLayoutType) {
 	return (
 		<html lang="kr">
 			<body>{children}</body>
