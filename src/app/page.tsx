8a047623414a40a6971ef70324586816156174b5
배성완 <baesee@kakao.com>
2023-12-30 23:39:13 +0900

    feat : server action (login,signin,logout) Relate to : #17

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import { cookies } from 'next/headers';
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
+
 export default function Home() {
+	const signIn = async (formData: FormData) => {
+		'use server';
+
+		const email = formData.get('email') as string;
+		const password = formData.get('password') as string;
+		const cookieStore = cookies();
+		const supabase = createClient(cookieStore);
+
+		const { error } = await supabase.auth.signInWithPassword({
+			email,
+			password,
+		});
+
+		if (error) {
+			return redirect('/login?message=Could not authenticate user');
+		}
+
+		return redirect('/');
+	};
 	return <main></main>;
 }
