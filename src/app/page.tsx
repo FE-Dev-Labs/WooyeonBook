25d0afb4b7f1e9ad5e9c9df4b195341b065a7f63
배성완 <baesee@kakao.com>
2024-01-02 11:05:41 +0900

    feat : sign, logout 추가 Relate to : #17

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { cookies } from 'next/headers';
+import { cookies, headers } from 'next/headers';
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 
@@ -17,10 +17,70 @@ export default function Home() {
 		});
 
 		if (error) {
-			return redirect('/login?message=Could not authenticate user');
+			return redirect('/?message=Could not authenticate user');
 		}
 
 		return redirect('/');
 	};
-	return <main></main>;
+
+	const signUp = async (formData: FormData) => {
+		'use server';
+
+		const origin = headers().get('origin');
+		const email = formData.get('email') as string;
+		const password = formData.get('password') as string;
+		const cookieStore = cookies();
+		const supabase = createClient(cookieStore);
+
+		const { error } = await supabase.auth.signUp({
+			email,
+			password,
+			options: {
+				emailRedirectTo: `${origin}/auth/callback`,
+			},
+		});
+
+		if (error) {
+			return redirect('/?message=Could not authenticate user');
+		}
+
+		return redirect('/?message=Check email to continue sign in process');
+	};
+
+	const signOut = async () => {
+		'use server';
+
+		const cookieStore = cookies();
+		const supabase = createClient(cookieStore);
+		await supabase.auth.signOut();
+		return redirect('/');
+	};
+
+	return (
+		<main>
+			<form action={signIn}>
+				<input type="email" name="email" placeholder="Email" required />
+				<input
+					type="password"
+					name="password"
+					placeholder="Password"
+					required
+				/>
+				<button formAction={signIn}>Sign In</button>
+			</form>
+			<form action={signUp}>
+				<input type="email" name="email" placeholder="Email" required />
+				<input
+					type="password"
+					name="password"
+					placeholder="Password"
+					required
+				/>
+				<button formAction={signUp}>Sign Up</button>
+			</form>
+			<form action={signOut}>
+				<button type="submit">Sign Out</button>
+			</form>
+		</main>
+	);
 }
