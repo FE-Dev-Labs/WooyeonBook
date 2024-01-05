0c83ef7db2dbd93a90c47a185ef3dfb7a62f9967
배성완 <baesee@kakao.com>
2024-01-06 02:53:36 +0900

    Troubleshooting : react-select 사용시 오류 - https://peaceful-play-cd8.notion.site/react-select-server-client-id-error-c1e6004c011a42998e5c542433683195\?pvs\=4
    Relate to : #1

--- src/components/community/common/ControlPanel.tsx
+++ src/components/community/common/ControlPanel.tsx
@@ -1,20 +1,23 @@
 'use client';
-import Select from 'react-select';
+import { usePathname } from 'next/navigation';
+import dynamic from 'next/dynamic';
+
 function ControlPanel() {
+	const Select = dynamic(() => import('react-select'), { ssr: false });
+
+	const pathName = usePathname();
 	const sortOptions = [
 		{ value: 'Latest', label: '최신순' },
 		{ value: 'Oldest', label: '오래된 순' },
 		{ value: 'View', label: '조회순' },
 	];
-	const option = [
-		{ value: '', label: '전체' },
-		{ value: 'Latest', label: '최신순' },
-	];
+	// console.log(pathName.split('/')[2]);
+
 	return (
 		<div>
 			<div>
 				<Select options={sortOptions} isSearchable={false} />
-				<Select options={option} isSearchable={false} />
+				<Select options={sortOptions} isSearchable={false} />
 			</div>
 			<button>글쓰기</button>
 		</div>
