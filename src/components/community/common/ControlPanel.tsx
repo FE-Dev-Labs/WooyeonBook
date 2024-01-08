59248db2772fda38d1e889c0bf045fbc7f09db10
배성완 <baesee@kakao.com>
2024-01-08 16:06:35 +0900

    design : controlPanel
    Relate to : #1

--- src/components/community/common/ControlPanel.tsx
+++ src/components/community/common/ControlPanel.tsx
@@ -1,7 +1,8 @@
 'use client';
 import { usePathname } from 'next/navigation';
 import dynamic from 'next/dynamic';
-
+import styles from '@/styles/community/controlPanel.module.css';
+import Link from 'next/link';
 function ControlPanel() {
 	const Select = dynamic(() => import('react-select'), { ssr: false });
 
@@ -13,44 +14,46 @@ function ControlPanel() {
 	];
 	const categoriesByPage = () => {
 		const page = pathName.split('/')[2];
-		if (page === 'bookReport') {
+		if (page === 'meeting') {
 			return [
-				{ value: 'All', label: '전체' },
 				{ value: 'All', label: '모집중' },
 				{ value: 'All', label: '모집완료' },
 			];
 		}
-		if (page === 'bookReport') {
+		if (page === 'buyingBook') {
 			return [
-				{ value: 'All', label: '전체' },
 				{ value: 'All', label: '삽니다' },
 				{ value: 'All', label: '거래완료' },
 			];
 		}
-		if (page === 'bookReport') {
+		if (page === 'sellingBook') {
 			return [
-				{ value: 'All', label: '전체' },
 				{ value: 'All', label: '나눔' },
 				{ value: 'All', label: '팝니다' },
 				{ value: 'All', label: '판매완료' },
 			];
 		}
 	};
-	const categoryOptions = [
-		{ value: 'All', label: '전체' },
-		{ value: 'Free', label: '자유' },
-		{ value: 'QnA', label: '질문' },
-	];
 
 	return (
-		<div>
-			<div>
-				<Select options={sortOptions} isSearchable={false} />
-				{pathName.split('/')[2] === 'bookReport' ? null : (
-					<Select options={categoryOptions} isSearchable={false} />
+		<div className={styles.container}>
+			<div className={styles.optionBtnWrap}>
+				<Select
+					className={styles.sortOptionBtn}
+					options={sortOptions}
+					defaultValue={sortOptions[0]}
+					isSearchable={false}
+				/>
+				{pathName && pathName.split('/')[2] === 'bookReport' ? null : (
+					<Select
+						className={styles.optionBtn}
+						options={categoriesByPage()}
+						defaultValue={'전체'}
+						isSearchable={false}
+					/>
 				)}
 			</div>
-			<button>글쓰기</button>
+			<Link href={``}>글쓰기</Link>
 		</div>
 	);
 }
