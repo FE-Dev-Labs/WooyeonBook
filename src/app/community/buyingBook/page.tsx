ce0d7137192ac15c038404675ae11b57328a3449
배성완 <baesee@kakao.com>
2024-01-08 16:35:41 +0900

    design : buyingBook, SellingBook, meeting page Relate to : #1

---
+++ src/app/community/buyingBook/page.tsx
@@ -0,0 +1,9 @@
+import ContentBox from '@/components/community/common/ContentBox';
+
+export default function buyingBook() {
+	return (
+		<div>
+			<ContentBox page={'meeting'} />
+		</div>
+	);
+}
---
+++ src/app/community/sellingBook/page.tsx
@@ -0,0 +1,9 @@
+import ContentBox from '@/components/community/common/ContentBox';
+
+export default function sellingBook() {
+	return (
+		<div>
+			<ContentBox page={'sellingBook'} />
+		</div>
+	);
+}
