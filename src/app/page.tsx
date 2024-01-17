64b1153de94081d8f0193c4d70f444dff13c8459
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2024-01-17 16:35:57 +0900

    install : react-slick, slick-carousel
    Related to : #18

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,15 @@ import NewBook from '../components/main/newBook/NewBook';
 import ThemeRecommendation from '@/components/main/themeRecommendation/ThemeRecommendation';
 import BestSeller from '@/components/main/bestSeller/BestSeller';
 import UsedBook from '@/components/main/usedBook/UsedBook';
+import dynamic from 'next/dynamic';
+import ImageSlider from '@/components/main/slider/ImageSlider';
 
 export default function Home() {
 	return (
 		<main className={styles.container}>
 			<RecentlyViewedBooks />
 			<div className={styles.wrapper}>
+				<ImageSlider />
 				<NewBook />
 				<ThemeRecommendation />
 				<BestSeller />
