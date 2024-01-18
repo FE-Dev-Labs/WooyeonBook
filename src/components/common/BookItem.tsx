b351b2de28d3ec03744b33d9c2b65da984677297
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2024-01-19 00:26:56 +0900

    design : bookItem component add, delete image
    main page 작업 시 놓쳤던 bookItem 내 addBook, deleteBook 이미지 및 상태 추가
    
    Related to : #19

--- src/components/common/BookItem.tsx
+++ src/components/common/BookItem.tsx
@@ -1,18 +1,25 @@
+'use client';
+
+import { useState } from 'react';
 import styles from '@/styles/common/bookItem.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 import bookImage from '../../../public/main/book3.png';
+import addBook from '../../../public/common/addBook.png';
+import deleteBook from '../../../public/common/deleteBook.png';
 
 interface BookItemProp {
 	rank?: number;
 }
 
 export default function BookItem({ rank }: BookItemProp) {
+	const [isAdded, setIsAdded] = useState(false);
+
 	return (
 		<div className={styles.bookItem}>
 			{rank && <div className={styles.rank}>{rank}</div>}
-			<Link href="/">
-				<div className={styles.bookImage}>
+			<div className={styles.bookImage}>
+				<Link href="/">
 					<Image
 						src={bookImage}
 						alt="new book"
@@ -20,8 +27,18 @@ export default function BookItem({ rank }: BookItemProp) {
 						// layout="fill"
 						// objectFit="cover"
 					/>
-				</div>
-			</Link>
+				</Link>
+			</div>
+			<div
+				className={styles.addDeleteBook}
+				onClick={() => setIsAdded(!isAdded)}>
+				<Image
+					src={isAdded ? deleteBook : addBook}
+					alt={isAdded ? 'Delete book' : 'Add book'}
+					width={30}
+					height={30}
+				/>
+			</div>
 			<div className={styles.bookText}>
 				<Link href="/">
 					<h1>베르베르씨, 오늘은 뭘 쓰세요?</h1>
