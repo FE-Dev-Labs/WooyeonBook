92df22358845e9803b66f9e1774170023f724596
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2024-01-20 19:35:34 +0900

    refactor : category component
    Related to : #42

--- src/app/best/page.tsx
+++ src/app/best/page.tsx
@@ -1,43 +1,14 @@
+import Category from '@/components/best/Category';
 import PageHeader from '@/components/common/PageHeader';
 import styles from '@/styles/best/best.module.css';
 
 export default function bestPage() {
-	const categoryItems = [
-		'전체',
-		'소설',
-		'에세이·시',
-		'경제·경영',
-		'자기개발',
-		'인문',
-		'사회·정치',
-		'역사',
-		'종교',
-		'예술·대중문화',
-		'자연과학',
-		'가정·살림',
-		'건강·취미·여행',
-		'어린이·유아',
-		'청소년',
-		'국어·외국어',
-		'IT·모바일',
-		'대학교재',
-		'수험서·자격증',
-		'잡지',
-		'만화',
-		'로맨스',
-		'판타지/무협',
-	];
-
 	return (
 		<>
 			<PageHeader title="베스트셀러" />
 			<div className={styles.container}>
 				<div className={styles.wrapper}>
-					<div className={styles.categoryBox}>
-						{categoryItems.map((item) => (
-							<div className={styles.categoryItem}>{item}</div>
-						))}
-					</div>
+					<Category />
 				</div>
 			</div>
 		</>
