54ccdf43d3300587e819ba768516c685a7bd5b92
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2024-01-20 20:33:54 +0900

    design : Sort component
    Related to : #42

--- src/app/best/page.tsx
+++ src/app/best/page.tsx
@@ -1,9 +1,15 @@
 import Category from '@/components/best/category/Category';
+import BookItem from '@/components/common/BookItem';
 import PageHeader from '@/components/common/PageHeader';
 import Sort from '@/components/common/Sort';
 import styles from '@/styles/best/best.module.css';
 
 export default function bestPage() {
+	let ranks = [];
+	for (let i = 1; i <= 30; i++) {
+		ranks.push(i);
+	}
+
 	return (
 		<>
 			<PageHeader title="베스트셀러" />
@@ -11,6 +17,14 @@ export default function bestPage() {
 				<div className={styles.wrapper}>
 					<Category />
 					<Sort />
+					<div className={styles.rankBox}>
+						{ranks.map((rank) => (
+							<div className={styles.itemWrapper} key={rank}>
+								<div className={styles.rank}>{rank}</div>
+								<BookItem />
+							</div>
+						))}
+					</div>
 				</div>
 			</div>
 		</>
