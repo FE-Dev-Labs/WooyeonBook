efef62a03619c5c0918b99d5fad42683fa225b75
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2024-01-20 20:12:04 +0900

    refactor : Sort component
    타 페이지에서 사용해야하는 Sort 컴포넌트 파일 리팩토링
    
    Related to : #42

--- src/app/best/page.tsx
+++ src/app/best/page.tsx
@@ -1,5 +1,6 @@
-import Category from '@/components/best/Category';
+import Category from '@/components/best/category/Category';
 import PageHeader from '@/components/common/PageHeader';
+import Sort from '@/components/common/Sort';
 import styles from '@/styles/best/best.module.css';
 
 export default function bestPage() {
@@ -9,13 +10,7 @@ export default function bestPage() {
 			<div className={styles.container}>
 				<div className={styles.wrapper}>
 					<Category />
-					<div className={styles.sortBox}>
-						<div className={styles.productAmounts}>상품 (123)</div>
-						<div className={styles.sortItem}>
-							<div>인기순</div>
-							<div>30</div>
-						</div>
-					</div>
+					<Sort />
 				</div>
 			</div>
 		</>
