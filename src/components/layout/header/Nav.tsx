9ba6b1af6c710c2ddf6102bd7f0e9a7aedfd1d05
배성완 <baesee@kakao.com>
2024-01-22 13:53:58 +0900

    design : 분류 드롭다운 구현 Relate to : #50

--- src/components/layout/header/Nav.tsx
+++ src/components/layout/header/Nav.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 
 export default function Nav() {
 	const nav = [
-		{ name: '분야', link: '/' },
 		{ name: '홈', link: '/' },
 		{ name: '신간도서', link: '/new' },
 		{ name: '테마추천', link: '/theme' },
@@ -11,9 +10,37 @@ export default function Nav() {
 		{ name: '중고도서', link: '/used' },
 		{ name: '커뮤니티', link: '/community' },
 	];
+	const category = [
+		{ name: '소설', link: '' },
+		{ name: '시/에세이', link: '' },
+		{ name: '경제/경영', link: '' },
+		{ name: '자기계발', link: '' },
+		{ name: '인문', link: '' },
+		{ name: '역사/문화', link: '' },
+		{ name: '사회/정치', link: '' },
+		{ name: '과학', link: '' },
+		{ name: '예술/대중문화', link: '' },
+		{ name: '종교', link: '' },
+		{ name: '가정/생활', link: '' },
+	];
 
 	return (
 		<div className={styles.navigationWrapper}>
+			<div className={styles.navigationCatagoryWrap}>
+				<div className={styles.navigationCatagoryItemWrap}>
+					분야 <p>▼</p>
+				</div>
+				<div className={styles.navigationMajorCatagoryWrap}>
+					<Link href={``} className={styles.allCatagoryItem}>
+						분야 전체 보기
+					</Link>
+					{category.map((item, index) => (
+						<Link key={index} href={item.link}>
+							{item.name}
+						</Link>
+					))}
+				</div>
+			</div>
 			{nav.map((item, index) => (
 				<Link key={index} href={item.link}>
 					{item.name}
