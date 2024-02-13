bccef955cd72d4d76eba345b787cdc24424eb385
FE_Wani <62400479+baesee0806@users.noreply.github.com>
2024-02-14 02:33:43 +0900

    design : header, title, loading 시에 보이는 화면
    Relate to : #67

--- src/app/community/(postUpdate)/post/new/page.tsx
+++ src/app/community/(postUpdate)/post/new/page.tsx
@@ -4,20 +4,46 @@ import styles from '@/styles/community/post/postNewPage.module.css';
 // dynamic import loading skeleton ui
 const Select = dynamic(() => import('react-select'), {
 	ssr: false,
-	loading: () => <p>로딩중...</p>,
+	loading: () => (
+		<div
+			style={{
+				width: '500px',
+				height: '38px',
+				backgroundColor: '#a5a5a5',
+				borderRadius: '5px',
+			}}></div>
+	),
 });
 const Editor = dynamic(
 	() => import('@/components/community/post/WysiwygEditor'),
 	{
 		ssr: false,
-		loading: () => <p>로딩중...</p>,
+		loading: () => (
+			<div
+				style={{
+					width: '1300px',
+					height: '601.25px',
+					backgroundColor: '#a5a5a5',
+					borderRadius: '5px',
+				}}></div>
+		),
 	},
 );
 export default function postNewPage() {
 	return (
 		<div className={styles.container}>
+			<div className={styles.header}>
+				<div>📚</div>
+				<h2>header 영역 입니다.</h2>
+			</div>
+			<input
+				type="text"
+				className={styles.title}
+				placeholder="제목을 입력해주세요."
+			/>
 			<div className={styles.optionWrap}>
 				<Select className={styles.bookSelectBtn} />
+				<div>이 책이 맞나용?</div>
 			</div>
 			<div>
 				<Editor />
