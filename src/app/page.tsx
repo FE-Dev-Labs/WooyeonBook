a2aabb549e0c92351b45506c0713dac0a8012e9e
Wonjun KIM <k1mwnjn@gmail.com>
2024-02-21 16:50:44 +0900

    design : BookItem
    책 타이틀 3줄 이상 노출 시 잘리는 글자 잘리는 부분이 생겨 디자인 수정함
    
    Related to : #72

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,13 +19,13 @@ export default async function Home() {
 
 	// 메인 페이지에 뿌려줄 신간 리스트 (6개)
 	const newBookData = await fetch(
-		`http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${process.env.NEXT_PUBLIC_TTB_KEY}&QueryType=ItemNewAll&MaxResults=6&start=1&SearchTarget=Book&output=js&Version=20131101&Cover=Big`,
+		`http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${process.env.NEXT_PUBLIC_TTB_KEY}&QueryType=ItemNewSpecial&MaxResults=6&start=1&SearchTarget=Book&output=js&Version=20131101&Cover=Big`,
 		{ cache: 'force-cache' },
 	).then((data) => {
 		return data.json();
 	});
 
-	// 메인 페이지에 뿌려줄 베스트셀러 리스트 (5개. *MaxResults=5일 경우 4개 아이템만 받아올 수 있어 MaxResults=6 사용 후 data를 잘라서 사용함 )
+	// 메인 페이지에 뿌려줄 베스트셀러 리스트 5개 (MaxResults=5일 경우 4개 아이템만 받아올 수 있어 MaxResults=6으로 받아온 후 data를 잘라서 사용함)
 	const bestSellerData = await fetch(
 		`http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${process.env.NEXT_PUBLIC_TTB_KEY}&QueryType=Bestseller&MaxResults=6&start=1&SearchTarget=Book&output=js&Version=20131101&Cover=Big`,
 		{ cache: 'force-cache' },
@@ -36,7 +36,7 @@ export default async function Home() {
 		.sort((a: any, b: any) => b.item?.bestRank - a.item?.bestRank)
 		.slice(0, 5);
 
-	// console.log(newBookData);
+	console.log(newBookData);
 	// console.log(bestSellerData.item.length);
 
 	return (
