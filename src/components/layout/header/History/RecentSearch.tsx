9f0f3d5e2ed4e2b65db6833f36e5271feffe8706
Jinyang-park <dbsskdud60@gmail.com>
2024-03-02 02:00:32 +0900

    feat : input에 onFocus로 변경,ref 이동
    Relate to :#85

--- src/components/layout/header/History/RecentSearch.tsx
+++ src/components/layout/header/History/RecentSearch.tsx
@@ -4,39 +4,27 @@ import LastestWord from './LastestWord';
 import HotWord from './HotWord';
 import Image from 'next/image';
 import cancelIcon from '../../../../../public/layout/cancel.png';
-import { forwardRef } from 'react';
 
-interface RecentSearchProps {
-	show: boolean;
-}
-
-const RecentSearch = forwardRef<HTMLDivElement, RecentSearchProps>(
-	({ show }, ref) => {
-		console.log('show확인용', show);
-		return (
-			<div
-				className={styles.recentSearchContainer}
-				ref={ref}
-				style={{ display: show ? 'block' : 'none' }}>
-				<div className={styles.recentSearchWrapper}>
-					<LastestWord />
-					<HotWord />
-					<div className={styles.lastlestDeleteAll}>
-						<span className={styles.lastelestDeleteAllText}>검색기록 삭제</span>
-						<div className={styles.lastlestCloseWrap}>
-							<span className={styles.lastelestCloseText}>닫기</span>
-							<Image
-								src={cancelIcon}
-								alt="cancelIcon"
-								width={10}
-								height={10}
-								className={styles.cancelIcon}
-							/>
-						</div>
+export default function RecentSearch() {
+	return (
+		<div className={styles.recentSearchContainer}>
+			<div className={styles.recentSearchWrapper}>
+				<LastestWord />
+				<HotWord />
+				<div className={styles.lastlestDeleteAll}>
+					<span className={styles.lastelestDeleteAllText}>검색기록 삭제</span>
+					<div className={styles.lastlestCloseWrap}>
+						<span className={styles.lastelestCloseText}>닫기</span>
+						<Image
+							src={cancelIcon}
+							alt="cancelIcon"
+							width={10}
+							height={10}
+							className={styles.cancelIcon}
+						/>
 					</div>
 				</div>
 			</div>
-		);
-	},
-);
-export default RecentSearch;
+		</div>
+	);
+}
--- src/components/layout/header/Search.tsx
+++ src/components/layout/header/Search.tsx
@@ -8,37 +8,33 @@ import useOutsideClick from '@/hooks/useOutsideClick';
 
 export default function Search() {
 	const ref = useRef<HTMLInputElement>(null);
-	const inputRef = useRef<HTMLInputElement>(null);
+
 	const [showSearchHistory, setShowSearchHistory] = useState(false);
 
 	useOutsideClick({
 		ref,
-		handler: (e) => {
-			if (e.target !== inputRef.current) {
-				setShowSearchHistory(false);
-			}
+		handler: () => {
+			setShowSearchHistory(false);
 		},
 	});
 
-	// const handleSearchClick = (e: React.MouseEvent<HTMLInputElement>) => {
-	// 	e.stopPropagation(); // 이벤트 버블링을 멈춤
-	// 	setShowSearchHistory(true); // 항상 true로 설정
-	// };
-	const handleSearchClick = () => {
+	const handleFocus = () => {
 		setShowSearchHistory(true);
 	};
+
 	return (
 		<span>
 			<form className={styles.searchForm}>
 				<input
 					type="text"
 					placeholder="작가명 또는 책 제목을 입력하세요."
-					onClick={handleSearchClick}
+					onFocus={handleFocus}
+					ref={ref}
 				/>
 				<button type="submit" className={styles.searchIcon}>
 					<Image src={searchIcon} alt="searchIcon" width={20} height={20} />
 				</button>
-				<RecentSearch ref={ref} show={showSearchHistory} />
+				{showSearchHistory && <RecentSearch />}
 			</form>
 		</span>
 	);
