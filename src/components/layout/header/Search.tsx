d23ff901e40b98861573a8d234f3d12404ece9a5
Jinyang-park <dbsskdud60@gmail.com>
2024-03-05 16:15:06 +0900

    feat : 자동 검색어 state 지정
    Relate to : #85

--- src/components/layout/header/Search.tsx
+++ src/components/layout/header/Search.tsx
@@ -7,10 +7,18 @@ import { useRef, useState } from 'react';
 import useOutsideClick from '@/hooks/useOutsideClick';
 
 export default function Search() {
+	// 외부 클릭 시
 	const ref = useRef<HTMLInputElement>(null);
-
+	// 최근 검색어, 인기 검색어
 	const [showSearchHistory, setShowSearchHistory] = useState(false);
+	// 자동 검색어
+	const [keyword, setKeyword] = useState<string>('');
+	// 검색창에 이벤트 조작을 이용하여 우리가 검색할 keyword변수
+	const onChangData = (e: React.FormEvent<HTMLInputElement>) => {
+		setKeyword(e.currentTarget.value);
+	};
 
+	// useOutsideClick 훅
 	useOutsideClick({
 		ref,
 		handler: () => {
@@ -18,6 +26,7 @@ export default function Search() {
 		},
 	});
 
+	// input 커서 시 RecentSearch 컴포넌트 나오는 로직
 	const handleFocus = () => {
 		setShowSearchHistory(true);
 	};
@@ -29,6 +38,7 @@ export default function Search() {
 					type="text"
 					placeholder="작가명 또는 책 제목을 입력하세요."
 					onFocus={handleFocus}
+					onChange={onChangData}
 					ref={ref}
 				/>
 				<button type="submit" className={styles.searchIcon}>
