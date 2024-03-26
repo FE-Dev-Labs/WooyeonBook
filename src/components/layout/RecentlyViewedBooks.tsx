d9acfa04983f99e6ae41c88696af39bc3d21aa1b
Wonjun KIM <k1mwnjn@gmail.com>
2024-03-27 00:01:41 +0900

    feat : common - RecentlyViewedBooks component
    페이지네이션 추가
    
    Related to : #118

--- src/components/layout/RecentlyViewedBooks.tsx
+++ src/components/layout/RecentlyViewedBooks.tsx
@@ -16,6 +16,32 @@ interface Item {
 export default function RecentlyViewedBooks() {
 	// 최근 본 상품 목록 state
 	const [recentItems, setRecentItems] = useState<Item[]>([]);
+	// 현재 카테고리의 현재 페이지 state
+	const [currentPage, setCurrentPage] = useState(0);
+
+	// 페이지 별로 아이템을 보여주기 위해 설정
+	const itemsPerPage = 3;
+	const totalPages = Math.ceil(recentItems.length / itemsPerPage);
+
+	// 현재 페이지에 따른 아이템을 계산하는 함수
+	const currentItems = recentItems.slice(
+		currentPage * itemsPerPage,
+		(currentPage + 1) * itemsPerPage,
+	);
+
+	// 페이지 변경 함수
+	const handleNextBtnClick = () => {
+		// 현재 페이지가 마지막 페이지보다 작을 때만 페이지 증가
+		if (currentPage < totalPages - 1) {
+			setCurrentPage((prev) => prev + 1);
+		}
+	};
+	const handlePrebBtnClick = () => {
+		// 현재 페이지가 첫 페이지보다 클 때만 페이지 감소
+		if (currentPage > 0) {
+			setCurrentPage((prev) => prev - 1);
+		}
+	};
 
 	// 로컬 스토리지에서 가져온 최근 본 상품을 렌더링해줄 useEffect
 	useEffect(() => {
@@ -30,9 +56,9 @@ export default function RecentlyViewedBooks() {
 	return (
 		<div className={styles.container}>
 			<div className={styles.wrapper}>
-				<div className={styles.textWrapper}>최근 본 상품</div>
+				<header className={styles.textWrapper}>최근 본 상품</header>
 				<div className={styles.booksWrapper}>
-					{recentItems.map((item) => (
+					{currentItems.map((item) => (
 						<Link
 							href={`/detail/${item?.itemIsbn}?type=${
 								item?.itemMallType === 'USED' ? 'used' : 'new'
@@ -40,20 +66,34 @@ export default function RecentlyViewedBooks() {
 							<Image
 								key={item.itemIsbn}
 								src={item.itemCover}
-								alt="book"
+								alt="recently viewed book"
 								width={60}
-								height={80}
+								height={79}
 							/>
 						</Link>
 					))}
 				</div>
 				<div className={styles.buttonWrapper}>
 					<div>
-						<Image src={arrowLeftIcon} alt="arrow" width={20} height={20} />
+						<Image
+							src={arrowLeftIcon}
+							alt="arrow"
+							width={20}
+							height={20}
+							onClick={handlePrebBtnClick}
+						/>
 					</div>
-					<p className={styles.buttonNumber}>1/3</p>
+					<p className={styles.buttonNumber}>
+						{currentPage + 1}/{totalPages}
+					</p>
 					<div>
-						<Image src={arrowRightIcon} alt="arrow" width={20} height={20} />
+						<Image
+							src={arrowRightIcon}
+							alt="arrow"
+							width={20}
+							height={20}
+							onClick={handleNextBtnClick}
+						/>
 					</div>
 				</div>
 			</div>
