7f7dcb7c34c9d659d2f77d7d4bd5a2da8e529e67
Wonjun KIM <k1mwnjn@gmail.com>
2024-03-29 22:34:28 +0900

    feat : detail page - 주문 기능 추가
    Related to : #128

--- src/components/detail/detaildescription/Detailorderbtn.tsx
+++ src/components/detail/detaildescription/Detailorderbtn.tsx
@@ -1,8 +1,11 @@
+import { getUser } from '@/apis/community/getUser';
 import { cartAtom } from '@/recoil/atom/cartAtom';
 import { itemAmountAtom } from '@/recoil/atom/itemAmountAtom';
 import styles from '@/styles/detail/detaildescription/detailorderbtn.module.css';
 import { Book } from '@/types/bookDetailDate';
 import { CartItemType } from '@/types/bookType';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 
 interface DetailOrderBtnProp {
@@ -10,31 +13,32 @@ interface DetailOrderBtnProp {
 }
 
 export default function Detailorderbtn({ bookInfo }: DetailOrderBtnProp) {
+	// useRouter 호출
+	const router = useRouter();
 	// 카트 아이템 state
 	const [cart, setCart] = useRecoilState<CartItemType[]>(cartAtom);
 	// 현재 카트 아이템의 수량
 	const itemQuantity = useRecoilValue(itemAmountAtom);
+	// 유저 state
+	const [isLoggedIn, setIsLoggedIn] = useState<string | null>(null);
+
+	// 카트페이지에서 필요한 요소들
+	const newCartItem = {
+		title: bookInfo.title,
+		author: bookInfo.author,
+		publisher: bookInfo.publisher,
+		priceSales: bookInfo.priceSales,
+		priceStandard: bookInfo.priceStandard,
+		isbn: bookInfo.isbn,
+		cover: bookInfo.cover,
+		quantity: itemQuantity,
+	};
+	// 장바구니에 같은 ISBN을 가진 책이 있는지 확인하는 함수
+	const isAlreadyInCart = cart.some((item) => item.isbn === newCartItem.isbn);
 
 	// 장바구니 담기 선택 시 동작하는 함수
 	const handleAddToCartClick = () => {
-		const newCartItem = {
-			// 카트페이지에서 필요한 요소들만 추출
-			title: bookInfo.title,
-			author: bookInfo.author,
-			publisher: bookInfo.publisher,
-			priceSales: bookInfo.priceSales,
-			priceStandard: bookInfo.priceStandard,
-			isbn: bookInfo.isbn,
-			cover: bookInfo.cover,
-			quantity: itemQuantity,
-		};
-
-		// 장바구니에 같은 ISBN을 가진 책이 있는지 확인
-		const isAlreadyInCart = cart.some((item) => item.isbn === newCartItem.isbn);
-		// 컨펌
-		const isConfirmed = confirm('선택 상품을 장바구니에서 담으시겠습니까?');
-		// 사용자가 '예'를 선택한 경우
-		if (isConfirmed) {
+		if (confirm('선택 상품을 장바구니에서 담으시겠습니까?')) {
 			// 이미 장바구니에 아이템이 존재하는 경우
 			if (isAlreadyInCart) {
 				alert('이미 장바구니에 담긴 제품입니다.');
@@ -45,12 +49,45 @@ export default function Detailorderbtn({ bookInfo }: DetailOrderBtnProp) {
 		}
 	};
 
+	// 주문하기 선택 시 동작하는 함수
+	const handleOrderClick = async () => {
+		if (!isLoggedIn) {
+			if (confirm('로그인이 필요한 서비스입니다 로그인 하시겠습니까?')) {
+				router.push('/login');
+				return;
+			}
+		}
+		if (isAlreadyInCart) {
+			alert('이미 장바구니에 담긴 제품입니다.');
+			setCart([...cart]);
+			router.push('/cart');
+		} else {
+			alert('장바구니 페이지로 이동합니다.');
+			setCart([...cart, newCartItem]);
+			router.push('/cart');
+		}
+	};
+
+	// 로그인 상태 변경해주는 useEffect
+	useEffect(() => {
+		const fetchUser = async () => {
+			const { user_id } = await getUser();
+			setIsLoggedIn(user_id ?? null);
+		};
+
+		fetchUser();
+	}, []);
+
+	console.log(isLoggedIn);
+
 	return (
 		<div className={styles.btnWrapper}>
 			<button className={styles.addToCartBtn} onClick={handleAddToCartClick}>
 				장바구니 담기
 			</button>
-			<button className={styles.orderBtn}>주문하기</button>
+			<button className={styles.orderBtn} onClick={handleOrderClick}>
+				주문하기
+			</button>
 		</div>
 	);
 }
