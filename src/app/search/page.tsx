b4b0b3b8d3c1baebfdae7d9b865ba48cc15f4555
Wonjun KIM <k1mwnjn@gmail.com>
2024-04-03 03:33:16 +0900

    refactor : search page
    Related to : #134

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -25,19 +25,6 @@ export default function searchPage() {
 	// 소팅 state(제목순, 최신순)
 	const sortType = useRecoilValue(sortTypeState);
 
-	// server -> api 받아오는 함수
-	const fetchData = async () => {
-		const response = await fetch(
-			`http://localhost:8080/list/search?query=${keyword}`,
-			// {
-			// 	cache: 'force-cache',
-			// },
-		);
-		const { data, dataLength } = await response.json();
-		setData(data);
-		setDataLength(dataLength);
-	};
-
 	// 각 페이지(숫자) 선택 시 실행되는 함수(페이지네이션)
 	const handlePageNumClick = (pageNum: number) => {
 		// 현재 페이지 숫자와 선택하려는 페이지 숫자가 같으면 리턴
@@ -45,7 +32,7 @@ export default function searchPage() {
 		// 현재 페이지 숫자 변경
 		setCurrentPage(pageNum);
 		// 페이지 선택시 페이지 상단으로 스크롤 이동
-		window.scrollTo({ top: 300, behavior: 'smooth' });
+		window.scrollTo({ top: 320, behavior: 'smooth' });
 	};
 
 	// 소팅한 data
@@ -59,9 +46,22 @@ export default function searchPage() {
 						new Date(b.pubDate).getTime() - new Date(a.pubDate).getTime(),
 				);
 
+	// server -> api 받아오는 함수
+	const fetchSearchData = async () => {
+		const response = await fetch(
+			`http://localhost:8080/list/search?query=${keyword}`,
+			{
+				cache: 'force-cache',
+			},
+		);
+		const { data, dataLength } = await response.json();
+		setData(data);
+		setDataLength(dataLength);
+	};
+
 	// fetchData 뿌려주는 useEffect
 	useEffect(() => {
-		fetchData();
+		fetchSearchData();
 	}, [keyword]);
 
 	// 페이지 첫 시작 데이터의 숫자, 24 = 카테고리 페이지에 나타낼 아이템 갯수
@@ -70,7 +70,6 @@ export default function searchPage() {
 	const endIndex = startIndex + 30;
 	// 해당 페이지에서 보여줄 데이터
 	const pageData = sortedData?.slice(startIndex, endIndex);
-
 
 	return (
 		<>
@@ -84,7 +83,6 @@ export default function searchPage() {
 					<div className={styles.container}>
 						<div className={styles.wrapper}>
 							<SortBar page="search" dataLength={dataLength} />
-							{/* <BookItemWrapper data={sortedData} currentPage={currentPage} /> */}
 							<SearchBookItemWrapper data={pageData} />
 							<Pagination
 								dataLength={dataLength}
