a9e3d6a2b0518816d424a59fa5439337aae736b9
Wonjun KIM <k1mwnjn@gmail.com>
2024-04-07 10:00:00 +0900

    fix : cartAtom
    localStrage is not defined 에러 발생으로 인한 타입 검사 추가
    
    Related to : #147

--- src/recoil/atom/cartAtom.ts
+++ src/recoil/atom/cartAtom.ts
@@ -2,9 +2,13 @@ import { CartItemType } from '@/types/bookType';
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 
+// 04-10 기존 나타나지 않던 "localStorage is not defined" 에러 메시지로 인한 추가 코드(SSR 환경이 아닌 브라우저 환경인지 타입 검사)
+const isBrowser = typeof window !== 'undefined';
+
 const { persistAtom } = recoilPersist({
 	key: 'recoil-persist', // 로컬 스토리지에 저장될 때 사용될 key
-	storage: localStorage, // 사용할 스토리지 타입(로컬)
+	// 04-10 기존 나타나지 않던 "localStorage is not defined" 에러 메시지로 인한 추가 코드(SSR 환경이 아닌 브라우저 환경인지 타입 검사)
+	storage: isBrowser ? localStorage : undefined, // 사용할 스토리지 타입(로컬)
 });
 
 export const cartAtom = atom<CartItemType[]>({
